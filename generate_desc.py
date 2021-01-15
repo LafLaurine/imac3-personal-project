aefee0bcb6b1ebddf3fe404293437328bdda1d9b
LafLaurine <laurine.lafontaine@outlook.fr>
2021-01-15 11:07:38 +0100

    [NLP] - Clean and better file structure

--- generate_desc.py
+++ generate_desc.py
@@ -1,32 +1,5 @@
-from pickle import load
 from numpy import argmax
 from keras.preprocessing.sequence import pad_sequences
-from keras.applications.vgg16 import VGG16
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
-from keras.applications.vgg16 import preprocess_input
-from keras.models import Model
-from keras.models import load_model
-
-# extract features from each photo in the directory
-
-
-def extract_features(filename):
-    # load the model
-    model = VGG16()
-    # re-structure the model
-    model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
-    # load the photo
-    image = load_img(filename, target_size=(224, 224))
-    # convert the image pixels to a numpy array
-    image = img_to_array(image)
-    # reshape data for the model
-    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-    # prepare the image for the VGG model
-    image = preprocess_input(image)
-    # get features
-    feature = model.predict(image, verbose=0)
-    return feature
 
 # map an integer to a word
 
@@ -64,16 +37,3 @@ def generate_desc(model, tokenizer, photo, max_length):
         if word == 'endseq':
             break
     return in_text
-
-
-# load the tokenizer
-tokenizer = load(open('tokenizer.pkl', 'rb'))
-# pre-define the max sequence length (from training)
-max_length = 34
-# load the model
-model = load_model('model-ep001-loss4.712-val_loss3.999.h5')
-# load and prepare the photograph
-photo = extract_features('example.jpg')
-# generate description
-description = generate_desc(model, tokenizer, photo, max_length)
-print(description)
---
+++ generate_new_desc.py
@@ -0,0 +1,15 @@
+import prepare_data
+import generate_desc
+from pickle import load
+from keras.models import load_model
+
+tokenizer = load(open('tokenizer.pkl', 'rb'))
+# pre-define the max sequence length (from training)
+max_length = 34
+# load the model
+model = load_model('model-ep001-loss4.712-val_loss3.999.h5')
+# load and prepare the photograph
+photo = prepare_data.extract_features_filename('example2.jpg')
+# generate description
+description = generate_desc.generate_desc(model, tokenizer, photo, max_length)
+print(description)
