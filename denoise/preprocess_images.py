0a4f85fc5e664ed91a426d674421c38b625a7d60
LafLaurine <laurine.lafontaine@outlook.fr>
2021-05-13 14:00:30 +0200

    [Denoise] - Change AE method

---
+++ denoise/preprocess_images.py
@@ -0,0 +1,12 @@
+import os
+import cv2
+import numpy as np
+ 
+
+for root, dirs, files in os.walk('../dataset/FlickerImages'):
+  for file in files:
+    img = cv2.imread(root + "/" + file)
+    gauss = np.random.normal(0,1,img.size)
+    gauss = gauss.reshape(img.shape[0],img.shape[1],img.shape[2]).astype('uint8')
+    noise = img + img * gauss
+    cv2.imwrite("../dataset/FlickerNoisyImages/"+file,noise)
